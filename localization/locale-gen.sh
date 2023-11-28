9c477b2bda1399d8a7d9a9f4017a1bcf17c8c863
badcast <lmecomposer@gmail.com>
2023-11-28 12:55:47 +0600

    Fix: locale-gen script Add: kz_KZ language

--- localization/locale-gen.sh
+++ localization/locale-gen.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
+function gen_locales()
+{
+    # Generate language (example, en_US, ru_RU)
+
+    for l in $@; do
+        echo Generating locale for $l
+        if [ ! -e "translates/$l.po" ]; then
+            msginit --locale=$l.UTF-8 --input=messages.pot --output=translates/$l.po
+        else
+            msgmerge --update translates/$l.po messages.pot
+        fi
+
+        # Gen machine (.mo)
+
+        mkdir -p ./locale/$l/LC_MESSAGES
+
+        msgfmt translates/$l.po -o ./locale/$l/LC_MESSAGES/dragon-tea.mo
+
+    done
+}
+
 find ../ ../src -type f -name "*.c" -o -name "*.h" | xargs xgettext -j --msgid-bugs-address="lmecomposer@gmail.com" --copyright-holder="badcast" --keyword=gettext --keyword=_ -o messages.pot --from-code=UTF-8 -d dragon-tea
 
-if [ ! -e "translates/ru_RU.po" ]; then
-    msginit --locale=ru_RU.UTF-8 --input=messages.pot --output=translates/ru_RU.po
-else
-    msgmerge --update translates/ru_RU.po messages.pot
-fi
-
-
-sh ./locale-gen-machine.sh
+gen_locales ru_RU kz_KZ
